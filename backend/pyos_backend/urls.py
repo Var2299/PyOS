c1704de2618235ac6f94063af7eda7425296f491
Varun Singh <vs95764@gmail.com>
2026-06-22 12:29:16 +0530

    Update urls.py

--- backend/pyos_backend/urls.py
+++ backend/pyos_backend/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('api/health/', health_check),
     path("api/auth/", include("apps.accounts.urls")),
     path("api/", include("apps.filesystem.urls")),
     path("api/", include("apps.core.urls")),
